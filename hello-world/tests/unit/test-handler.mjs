d8672256e53db1456b4ec0fd65bed3cdd9add940
Sam Wilson <samtwil@live.com>
2026-08-17 15:31:34 -0500

    Refactor test case for lambdaHandler response

--- hello-world/tests/unit/test-handler.mjs
+++ hello-world/tests/unit/test-handler.mjs
@@ -4,17 +4,17 @@ import { lambdaHandler } from '../../app.mjs';
 import { expect } from 'chai';
 var event, context;
 
-describe('Tests index', function () {
-    it('verifies successful response', async () => {
-        const result = await lambdaHandler(event, context)
+describe("Tests index", function () {
+  it("verifies successful response", async () => {
+    const result = await lambdaHandler(event, context)
 
-        expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
+    expect(result).to.be.an("object")
+    expect(result.statusCode).to.equal(200)
+    expect(result.body).to.be.an("string")
 
-        let response = JSON.parse(result.body);
+    let response = JSON.parse(result.body)
 
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello my friend");
-    });
-});
+    expect(response).to.be.an("object")
+    expect(response.message).to.be.equal("I'm using canary deployments")
+  })
+})
